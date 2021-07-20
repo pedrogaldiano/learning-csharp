92f84e2a0ffb34d110ad00744c9b9ef817ec73b9
Pedro Galdiano <pedrocastro014@gmail.com>
2021-07-20 09:15:40 -0300

    learning..

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # learning-csharp
 Keeping track of my learning in C#
-
-Just checking if the git is connected properly :3
---
+++ csintro/IntroUI/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            string cor = "Amarelo";
+            switch(cor)
+            {
+                case "Vermelho":
+                    Console.WriteLine("Vermelho");
+                    break;
+                case "Amarelo":
+                    Console.WriteLine("Amarelo");
+                    break;
+                default:
+                    Console.WriteLine("Default");
+                    break;
+            }
+
+            
+
+
+            // Console.ReadLine();
+        }
+    }
+}
+
