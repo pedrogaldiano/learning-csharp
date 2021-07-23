8aadcc8aea2aed46fc4e28ae73e8655f2d12cacb
Pedro Galdiano <pedrocastro014@gmail.com>
2021-07-23 08:24:00 -0300

    Update

---
+++ testcs/TestProject/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Solution 
+{
+    public int MaximumWealth(int[][] accounts) 
+    {
+        int richest = 0;
+        for (int i = 0; i < accounts.Length; i ++)
+        {
+            int sum = 0;
+            for (int j = 0; j < accounts[i].Length; j++)
+            {
+                sum += accounts[i][j];
+            }
+            if (sum > richest)
+            {
+                richest = sum;
+            }
+        }
+        return richest;
+    }
+}
