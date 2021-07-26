f08881032ce0691bbe89abc56051498bbb4415aa
Pedro Galdiano <pedrocastro014@gmail.com>
2021-07-26 14:30:25 -0300

    Number Guessing

---
+++ NumberGuessing/Project/Program.cs
@@ -0,0 +1,98 @@
+﻿/* 
+Number Guessing Game, where if the person guess wrong
+the computer will tell if you should guess a number higher or lower.
+
+It should be possible to run a computer vs computer game.
+*/
+
+using System;
+using System.Threading;
+
+
+namespace Project
+{
+    enum State
+    {
+        lower,
+        higher,
+        right
+    }
+    class Program
+    {   
+        static void PrintRange(int min, int max)
+        {
+            Console.WriteLine($"I'm thinking in a number between {min} and {max}, make a guess!");
+        }
+        static int InputUser(int min, int max)
+        {   
+            int input;
+            do
+            {
+                string s = Console.ReadLine();
+                if (!int.TryParse(s, out input))
+                {
+                    Console.WriteLine("Guess an Integer!");
+                }
+            }
+            while (input < min || input > max);
+
+            return input;
+        }
+        static State CheckInput(int input, int rnd)
+        {
+            if (input > rnd)
+            {
+                Console.WriteLine("Guess a lower number!");
+                return State.lower;
+            }
+            else if (input < rnd)
+            {
+                Console.WriteLine("Guess a higher number!");
+                return State.higher;
+            }
+            else
+            {
+                Console.WriteLine("YOU GUESSED RIGHT!");
+                return State.right;
+            }
+        }
+        static int Computer(int input)
+        {
+            Console.WriteLine(input);
+            return input;
+            Thread.Sleep(50);
+        }
+        static void Main(string[] args)
+        {   int min = 0;
+            int max = 100;
+            Random rnd = new Random();
+            int number = (int)rnd.Next(min, max);
+
+            bool key = true;
+
+            while (key)
+            {
+                PrintRange(min, max);
+
+                // int input = InputUser(min, max); //User plays
+                int input = Computer((int)rnd.Next(min, max)); //Computer plays
+                State result = CheckInput(input, number);
+
+                // Console.ReadLine();
+
+                switch (result)
+                {
+                    case State.higher:
+                        min = input;
+                        break;
+                    case State.lower:
+                        max = input;
+                        break;
+                    case State.right:
+                        key = false;
+                        break;
+                }
+            }
+        }
+    }
+}
--- testcs/TestProject/Program.cs
+++ testcs/TestProject/Program.cs
@@ -1,20 +1,68 @@
-﻿public class Solution 
+﻿using System;
+using System.Collections.Generic;
+
+namespace testcs
 {
-    public int MaximumWealth(int[][] accounts) 
+        class Account
     {
-        int richest = 0;
-        for (int i = 0; i < accounts.Length; i ++)
-        {
-            int sum = 0;
-            for (int j = 0; j < accounts[i].Length; j++)
-            {
-                sum += accounts[i][j];
-            }
-            if (sum > richest)
-            {
-                richest = sum;
-            }
-        }
-        return richest;
+        public string Name = "keke";
+    } ;
+    
+    class StructsAndObjectsDemo
+    {
+    public static void Main ()
+    {
+        Account RobsAccount ;
+        RobsAccount = new Account();
+        // RobsAccount.Name = "Rob";
+        Console.WriteLine (RobsAccount.Name );
     }
 }
+    // class LocalExample
+    // {
+    //     static void Main()
+    //     {
+    //         string a = " 000";
+    //         string b = "abcd";
+    //         char c = b[0];
+    //         a[0] = c;
+    //         Console.WriteLine(a);
+    //         // string s = "codeleet";
+    //         // int[] indices = new int[] {4,5,6,7,0,2,1,3};
+            
+    //         // string r = s;
+    //         // for (int i = 0; i < indices.Length; i++)
+    //         // {
+    //         //     r[i] = s[indices[i]];
+    //         // }
+
+    //         // Console.WriteLine(r);
+    //     }
+    // }
+}
+
+            // string j = "ABCDEFG";
+
+            // for (int i = 0; i < j.Length; i++)
+            // {
+            //     Console.WriteLine(j[i]);
+            // }
+            
+            // bool x = j.Contains(j[0]);
+
+            // Console.WriteLine(x);
+            // string[] abc = new string[] {
+            //     "A0", "B1", "C2", "D3", "E4", "F5", 
+            //      "--G6", "--H7", "--I8", "--J9", "--K10", "--L11"};
+
+          
+            // int[,] a = new int[5,3] {{1,2,3}, {4,5,6}, {7,8,9}, {10,11,12}, {13,14,15}};
+
+            // for (int i = 0; i < a.Length; i++)
+            // {
+            //     foreach (var j in a[0])
+            //     {
+            //         Console.Write(j +  "  ");
+            //     }
+            //     Console.WriteLine();
+            // }
